b6db6d85c3fae6c99fa3b9fbba6f591d2dbe871e
Gliverson Almeida <gliverson2014@gmail.com>
2021-08-16 18:08:57 -0300

    :rocket: create component card   🔨

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,9 +2,26 @@ import "./style.css";
 
 function CardGame() {
   return `
-    <article class="card-game">
-      <img src="images/alura-pixel.png" alt="Logo da Alura">
-    </article>
+    <div class="card">
+      <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+      </article>
+      <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+      </article>
+      <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+      </article>
+      <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+      </article>
+      <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+      </article>
+      <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+      </article>
+    </div>
   `;
 }
 
